911a741d3fdc2a31778010f6ecee9ee131564d0a
Febri Hardiyanto <73746901+febiens24@users.noreply.github.com>
2023-07-30 09:03:37 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+class Button extends React.Component{
+render(){
+return (
+<button className="h-10 px-6 font-semibold rounded-md bg-black text-white" type="submit">
+          Buy now
+      </button>
+);
+}
+}
+
+function App() {
+  return  <div className='flex justify-center bg-yellow-600 min-h-screen items-center'>
+    <div> 
+      <Button></Button>
+      <Button></Button>
+    </div>
+      
+  </div>;
+}
+
+export default App
+
