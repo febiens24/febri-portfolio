4c80f7f19bcb7509186df33f64211716cd3f5e2f
Febri Hardiyanto <73746901+febiens24@users.noreply.github.com>
2023-07-31 17:13:59 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
 import React from "react";
 
-class Button extends React.Component{
-render(){
-return (
+class Button extends React.Component {
+  render() {
+    return (
       <button className="h-10 px-6 font-semibold rounded-md bg-black text-white" type="submit">
-          Buy now
+        Buy now
       </button>
-);
-}
+    );
+  }
 }
 
 function App() {
-  return  <div className='flex justify-center bg-yellow-600 min-h-screen items-center'>
-    <div> 
+  return <div className='flex justify-center bg-yellow-600 min-h-screen items-center'>
+    <div>
       <Button></Button>
       <Button></Button>
     </div>
-      
+
   </div>;
 }
 
